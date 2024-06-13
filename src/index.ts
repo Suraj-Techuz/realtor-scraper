5eef5024f895ab62da63eedc6cfd7220610c391b
suraj <suraj@techuz.com>
2024-06-13 13:02:22 +0530

    stack size changed

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(express.json());
 
 const queue: { req: Request; res: Response }[] = [];
-const maxConcurrentRequests = 10;
+const maxConcurrentRequests = 2;
 let activeRequests = 0;
 
 const processQueue = async () => {
